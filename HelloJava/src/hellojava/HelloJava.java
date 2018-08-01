cdb6ec0fc2d6f09a09abe7e8991c336ddac40dfa
Aluno <Aluno>
2018-08-01 19:56:03 -0300

    Adicionado sysout com aula de psw

--- HelloJava/src/hellojava/HelloJava.java
+++ HelloJava/src/hellojava/HelloJava.java
@@ -16,6 +16,7 @@ public class HelloJava {
      */
     public static void main(String[] args) {
         System.out.println("Olá mundo");
+        System.out.println("Aula de PSW");
     }
     
 }
